64a5a7775bc382320eeb54427ac02dc11c5337a6
Chris Kostiw <104078019+ckostiw@users.noreply.github.com>
2022-05-09 16:12:14 -0400

    container, individual, comments extraction

--- src/moh/sample_manifest_extractor.py
+++ src/moh/sample_manifest_extractor.py
@@ -1,14 +1,15 @@
 from types import SimpleNamespace
+import re
 from pandas import pandas as pd
 from core.conversion_log import ConversionLog
 from freezeman.freezeman_config import CONTAINER_KIND, HEADERS as FMS_HEADERS
-from freezeman.sample import FreezemanSample
 from .moh_config import (
     HEADERS,
     SAMPLE_TYPES,
     CONTAINER_TYPES,
     TISSUE_TYPE_MAP,
     NUCLEIC_ACID_TYPE_MAP,
+    TAXON_TYPE_MAP,
 )
 from .sample_manifest import MOHSampleManifest
 
@@ -17,9 +18,20 @@ from .sample_manifest import MOHSampleManifest
 MOHSampleTypes = SimpleNamespace(**SAMPLE_TYPES)
 MOHHeaders = SimpleNamespace(**HEADERS)
 MOHContainerTypes = SimpleNamespace(**CONTAINER_TYPES)
+MOHTaxonTypes = SimpleNamespace(**TAXON_TYPE_MAP)
 
 FMSHeaders = SimpleNamespace(**FMS_HEADERS)
 FMSContainerKind = SimpleNamespace(**CONTAINER_KIND)
+
+
+def normalize_name(name):
+    """
+    Replace illegal characters by underscores.
+    Legal characters are a-z, A-Z, 0-9, _ (underscore), - (dash), . (period)
+    """
+
+    reg_ex = "[^a-z|A-Z|0-9|_.-]"
+    return re.sub(reg_ex, "_", name)
 
 
 class MOHSampleManifestExtractor:
@@ -47,20 +59,39 @@ class MOHSampleManifestExtractor:
 
         return samples
 
+    def _create_fms_sample(self):
+        """A sample is modeled as a dictionary with keys matching the keys in the fms header
+        definitions, wrapped in a SimpleNamespace to allow dot addressing of sample fields
+        """
+        sample = {}
+
+        for key in FMS_HEADERS.keys():
+            sample[key] = None
+
+        return SimpleNamespace(**sample)
+
     def _extract_sample_from_row(self, row):
 
         row_type = self._extract_sample_type(row)
         if row_type is None:
             return None
 
-        sample = FreezemanSample()
+        sample_ns = self._create_fms_sample()
 
-        sample.alias = row[MOHHeaders.SAMPLE_NAME]
-        sample.name = row[MOHHeaders.SAMPLE_NAME]
-        sample.kind = row_type
-        sample.volume = self._extract_value(row, MOHHeaders.VOLUME)
+        sample_ns.SAMPLE_KIND = row_type
 
-        return sample
+        sample_ns.SAMPLE_NAME = normalize_name(row[MOHHeaders.SAMPLE_NAME])
+        sample_ns.ALIAS = row[MOHHeaders.SAMPLE_NAME]
+
+        sample_ns.VOLUME = self._extract_value(row, MOHHeaders.VOLUME)
+
+        self._extract_container(row, sample_ns)
+
+        self._extract_individual(row, sample_ns)
+
+        sample_ns.COMMENT = self._extract_value(row, MOHHeaders.COMMENTS)
+
+        return sample_ns
 
     def _extract_value(self, row, header_name):
         value = row[header_name]
@@ -109,49 +140,53 @@ class MOHSampleManifestExtractor:
 
         return None
 
-    def _extract_container(self, row, sampleDict):
-
-        container_name = self._extract_value(row, MOHHeaders.CONTAINER_NAME)
-        if container_name == None:
-            self._log_warning("Container name is not specified")
-
-        container_barcode = self._extract_value(row, MOHHeaders.CONTAINER_BARCODE)
-        if container_barcode == None:
-            self._log_warning("Container barcode is not specified")
+    def _extract_container(self, row, sample_ns):
 
         container_type = self._extract_value(row, MOHHeaders.CONTAINER_TYPE)
-        if container_type == None:
+        if container_type is None:
             self._log_warning("Container type is not specified")
-        elif not container_type in CONTAINER_TYPES:
+        elif not container_type in CONTAINER_TYPES.values():
             self._log_error(f"Container type is not supported: {container_type}")
-
-        well = self._extract_value(row, MOHHeaders.WELL)
-
-        container_coord = None
-        sample_coord = None
-        container_kind = None
 
         if container_type == MOHContainerTypes.TUBE:
             # The well column contains the coord of the tube in its tube stand
-            container_coord = well
-            container_kind = FMSContainerKind.TUBE
+            sample_ns.CONTAINER_COORD = self._extract_value(row, MOHHeaders.WELL)
+            sample_ns.CONTAINER_KIND = FMSContainerKind.TUBE
         elif container_type == MOHContainerTypes.WELL_PLATE_96:
-            sample_coord = well
-            container_kind = FMSContainerKind.WELL_PLATE_96
+            sample_ns.SAMPLE_COORD = self._extract_value(row, MOHHeaders.WELL)
+            sample_ns.CONTAINER_KIND = FMSContainerKind.WELL_PLATE_96
         elif container_type == MOHContainerTypes.WELL_PLATE_384:
-            sample_coord = well
-            container_kind = FMSContainerKind.WELL_PLATE_384
+            sample_ns.SAMPLE_COORD = self._extract_value(row, MOHHeaders.WELL)
+            sample_ns.CONTAINER_KIND = FMSContainerKind.WELL_PLATE_384
+        else:
+            self._log_error(f"Container type is not supported: {container_type}")
 
-        # populate the sampleDict values for containers
-        # sampleDict[FMSHeaders.CONTAINER_NAME]
+        sample_ns.CONTAINER_NAME = self._extract_value(row, MOHHeaders.CONTAINER_NAME)
+        sample_ns.CONTAINER_BARCODE = self._extract_value(
+            row, MOHHeaders.CONTAINER_BARCODE
+        )
 
-        # Freezeman:
-        # container kind
-        # container name
-        # container barcode
-        # container coord
-        #
-        # location barcode
-        #
+        # TODO Which warning/error messages would be useful here, and which would just be noise?
 
-        return None
+    def _extract_individual(self, row, sample_ns):
+        # Individual ID
+        # Cohort ID
+        # Species
+        # Sex
+        sample_ns.INDIVIDUAL_ID = self._extract_value(row, MOHHeaders.INDIVIDUAL_ID)
+        if sample_ns.INDIVIDUAL_ID is None:
+            # The individual ID must be specified if any of the other individual fields
+            # are used.
+            return
+
+        sample_ns.COHORT = self._extract_value(row, MOHHeaders.COHORT_ID)
+
+        sample_ns.SEX = self._extract_value(row, MOHHeaders.SEX)
+
+        # For species, freezeman only supports a limited set of taxon id's
+        # Should we copy the taxon anyway and let the user decide what they want to do?
+        taxon = self._extract_value(row, MOHHeaders.SPECIES)
+        if taxon is not None:
+            sample_ns.TAXON = TAXON_TYPE_MAP[taxon]
+            if sample_ns.TAXON is None:
+                self._log_warning(f"Unsupported taxon type: {taxon}")
