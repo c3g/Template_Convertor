f87911a0aa3b92023c37bda8f32f6c72fc0a9e9b
Chris Kostiw <christopher.kostiw@mcgill.ca>
2022-06-03 07:40:56 -0400

    Fix line numbers

--- src/moh/sample_manifest.py
+++ src/moh/sample_manifest.py
@@ -10,6 +10,18 @@ class MOHSampleManifest:
         self.data_frame = data_frame
         self.cleaned_headers = list(map(lambda h: h.casefold(), self.headers))
 
+        # Using iloc to reference rows is a problem because when we iterate over rows
+        # pandas ignores rows with NaN values. There are two empty rows in the manifest
+        # header, and this throws line numbering off by two.
+        # Force a line number index onto the rows, and then reference the rows by
+        # this line number index. This ensures there is a line number on every row,
+        # including empty rows.
+        num_rows = self.data_frame.shape[0]
+        # create a line number array to use as index, starting at 1
+        line_nums = [n for n in range(1, num_rows+1)]
+        # set the index
+        self.data_frame.set_index(pd.Index(line_nums), drop=False, inplace=True)
+
         self.header_row_index = self._find_header_row_index()
         if self.header_row_index == -1:
             raise ManifestError(
@@ -17,13 +29,13 @@ class MOHSampleManifest:
             )
 
     def get_data_row_range(self):
-        """Get the index of the first data row in the sheet"""
+        """Get the range of line numbers for the data rows in the sheet"""
         numRows = self.data_frame.shape[0]
-        return range(self.header_row_index + 1, numRows)
+        return range(self.header_row_index + 1, numRows + 1)
 
-    def get_row(self, index):
-        return self.data_frame.iloc[index]
-        # return self.dataFrame.iloc[index: index+1, 0:]
+    def get_row(self, line_number):
+        # Use loc to get the row with the line number index we set on the row in the constructor
+        return self.data_frame.loc[line_number]
 
     def is_header_row(self, rowTuple):
         MAX_MISMATCHED_HEADER_CELLS = 3
@@ -58,21 +70,20 @@ class MOHSampleManifest:
 
     def _find_header_row_index(self):
         MAX_ROWS_TO_CHECK = 10
-        row_number = 0
-        for row in self.data_frame.itertuples(index=False, name=None):
-            matched_headers = self.is_header_row(row)
+        for row in self.data_frame.itertuples(index=True, name=None):
+            # the line number is first item in tuple
+            line_number = row[0]
+            # pass the row, excluding first line number element in the tuple
+            matched_headers = self.is_header_row(row[1:])
             if matched_headers is not None:
-
                 header_count = len(matched_headers)
                 while header_count < len(self.data_frame.columns):
-                    matched_headers[header_count] = self.data_frame.columns[
-                        header_count
-                    ]
+                    matched_headers[header_count] = self.data_frame.columns[header_count]
                     header_count += 1
 
                 self.data_frame.columns = matched_headers
-                return row_number
-            row_number += 1
-            if row_number > MAX_ROWS_TO_CHECK:
+                return line_number
+            # Abort if we can't find the header row within the first MAX_ROWS_TO_CHECK rows of the sheet.
+            if line_number > MAX_ROWS_TO_CHECK:
                 break
         return -1
