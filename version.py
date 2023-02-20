71642d50504776b1b619484db07a89fb574f0cbd
Chris Kostiw <104078019+ckostiw@users.noreply.github.com>
2023-02-20 09:20:22 -0500

    Updated for freezeman 4.0 (#26)

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
-CONVERTOR_VERSION = '3.13.0'
+CONVERTOR_VERSION = '4.0'
 
 # 3.12.1: Updated streamlit and Pillow because of security warnings
 # 3.13.0: Updated for freezeman 3.13.0 submission template
+# 4.0: Updated for freezeman 4.0 submission template
