30e444ffbdfbdc6e2891470469596e176198549b
Chris Kostiw <104078019+ckostiw@users.noreply.github.com>
2023-05-16 13:48:24 -0400

    Update with 4.2 template (#29)

--- version.py
+++ version.py
@@ -1,6 +1,7 @@
-CONVERTOR_VERSION = '4.1'
+CONVERTOR_VERSION = '4.2'
 
 # 3.12.1: Updated streamlit and Pillow because of security warnings
 # 3.13.0: Updated for freezeman 3.13.0 submission template
 # 4.0: Updated with freezeman 4.0 submission template
 # 4.1: Updated with freezeman 4.1 submission template
+# 4.2: Updated with freezeman 4.2 submission template
