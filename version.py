5fdc3f61a416d25d5fa0179f9b3e51efa1eb2799
Chris Kostiw <christopher.kostiw@mcgill.ca>
2023-08-15 14:40:41 -0400

    Just a version number update

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-CONVERTOR_VERSION = '4.3.1'
+CONVERTOR_VERSION = '4.4'
 
 # 3.12.1: 	Updated streamlit and Pillow because of security warnings
 # 3.13.0: 	Updated for freezeman 3.13.0 submission template
@@ -8,3 +8,4 @@ CONVERTOR_VERSION = '4.3.1'
 # 4.3:		Updated with freezeman 4.3 submission template
 # 			Added support for new fms "Sample Type" column
 # 4.3.1:	Handle numeric names properly
+# 4.4:		Updated version number only - no template changes
